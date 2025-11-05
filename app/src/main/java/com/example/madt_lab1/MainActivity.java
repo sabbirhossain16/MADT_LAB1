580186f4b255e868ef4790b55f9154f6533cbf98
sabbirhossain200424-svg <sabbirhossain200424@gmail.com>
2025-11-05 21:36:31 +0200

    Added button click event to change text

--- app/src/main/java/com/example/madt_lab1/MainActivity.java
+++ app/src/main/java/com/example/madt_lab1/MainActivity.java
@@ -1,24 +1,21 @@
 package com.example.madt_lab1;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.buttonChangeText);
+
+        button.setOnClickListener(v -> textView.setText("Button Clicked!"));
+
+
     }
 }
